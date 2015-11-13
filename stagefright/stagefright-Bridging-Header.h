21d15f4d3b791af2a0281a960797d8006cb22f27
Troy Shields <troy.leland.shields@gmail.com>
2015-11-13 00:17:46 -0800

    Adding google analytics. Seems like it is working but im not seeing any activity on analytics portal. Will check tomorrow

---
+++ stagefright/stagefright-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  stagefright-Bridging-Header.h
+//  stagefright
+//
+//  Created by Troy Shields on 11/13/15.
+//  Copyright © 2015 shldz. All rights reserved.
+//
+
+#ifndef stagefright_Bridging_Header_h
+#define stagefright_Bridging_Header_h
+
+#import <Google/Analytics.h>
+
+
+#endif /* stagefright_Bridging_Header_h */
---
+++ stagefright/stagefright/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  stagefright
+//
+//  Created by Troy Shields on 11/12/15.
+//  Copyright © 2015 shldz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        let name = "Pattern~\(self.title!)"
+        
+        // The UA-XXXXX-Y tracker ID is loaded automatically from the
+        // GoogleService-Info.plist by the `GGLContext` in the AppDelegate.
+        // If you're copying this to an app just using Analytics, you'll
+        // need to configure your tracking ID here.
+        // [START screen_view_hit_swift]
+        let tracker = GAI.sharedInstance().defaultTracker
+        tracker.set(kGAIScreenName, value: name)
+        
+        let builder = GAIDictionaryBuilder.createScreenView()
+        tracker.send(builder.build() as [NSObject : AnyObject])
+    }
+
+
+}
+
